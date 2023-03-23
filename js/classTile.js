c92e90c3793e9391a5f61c986032007d150a79a3
chris <you@example.com>
2023-03-23 10:50:06 +0200

    separate grid.js

--- js/classTile.js
+++ js/classTile.js
@@ -1,24 +1,42 @@
-class Tile{
-    constructor (x,y){
-      this.x=x;
-      this.y=y;
-      this.tileColor="gray";
-      this.centerX= this.x*columnWidth-1 + columnWidth/2;
-      this.centerY= this.y*rowHeight-1 + rowHeight/2;
-      }
-      
-
-  
-      paintSquare(){
-        ctx.fillStyle= this.tileColor;
-        ctx.fillRect(this.x*columnWidth, this.y*rowHeight, columnWidth, rowHeight);
-
-        // marks the center of the tile
-        ctx.fillStyle= "red";
-        ctx.fillRect(this.centerX, this.centerY, 2, 2);
-      }
-  
-      printTile(){
-        console.log(this.x,this.y)
-      }
+class Tile {
+  constructor({ position }) {
+    this.position = position;
+    this.tileColor = "gray";
+    this.centerX = this.position.x * columnWidth - 1 + columnWidth / 2;
+    this.centerY = this.position.y * rowHeight - 1 + rowHeight / 2;
   }
+
+  paintSquare() {
+    ctx.fillStyle = this.tileColor;
+    ctx.fillRect(this.position.x * columnWidth, this.position.y * rowHeight, columnWidth, rowHeight);
+
+    // marks the center of the tile
+    ctx.fillStyle = "red";
+    ctx.fillRect(this.centerX, this.centerY, 2, 2);
+  }
+
+  printTile() {
+    console.log(this.position.x, this.position.y);
+  }
+}
+
+function setUpTiles() {
+  tiles = [];
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < columns; j++) {
+      tiles.push(
+        new Tile({
+          position: {
+            x: j,
+            y: i,
+          },
+        })
+      );
+    }
+  }
+  //console.log(tiles.length);
+}
+
+function assignTile(x, y, tilearray) {
+  return tilearray.find((item) => item.position.x === x && item.position.y === y);
+}
